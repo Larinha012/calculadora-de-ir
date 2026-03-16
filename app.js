0e6400436d7483cee7a4d3a7582b647d92b70ef2
marcL2010 <marcia.7986@colegioprogresso.g12.br>
2026-03-16 10:14:15 -0300

    finalizamos

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ function calcularovalordosalario() {
     const valor = Number(valorsalario.value)
 
     if (valor <= 2000) {
-        display.innerHTML = `<p> Você esta isento </p>`
+        display.innerHTML = `<p> Você está isento </p>
+        <p> O salário informado foi ${valor}</p>`
     }else if (valor > 2000 && valor <= 3500) {
        const imposto = valor* 7.5 /100 
-       display.innerHTML = `<p> O valor do imposto é ${imposto}</p>` 
-        `<p>O salário informado foi ${valor}</p>`
+       display.innerHTML = `<p> O valor do imposto é ${imposto}</p>
+        <p>O salário informado foi ${valor}</p>
+        <p> O percentual aplicado é de 7.5%</p>`
     }else if (valor > 3500 && valor <= 5000) {
         const imposto = valor* 15 /100 
-        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>`
-        `<p> O salário informado foi ${valor}</p>`
+        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>
+        <p> O salário informado foi ${valor}</p>
+        <p> O percentual aplicado é de 15%</p>`
     }else{
         const imposto = valor* 22.5/100
-        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>`
-        `<p> O salário informado foi ${valor}</p>`
+        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>
+        <p> O salário informado foi ${valor}</p>
+        <p> O percentual aplicado é de 22.5%</p>`
     }
 
 }
