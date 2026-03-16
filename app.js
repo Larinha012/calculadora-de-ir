bb5a20092291e810b2d937ed8c451fc680e572ee
marcL2010 <marcia.7986@colegioprogresso.g12.br>
2026-03-16 08:36:37 -0300

    quase terminando

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const valorsalario = document.getElementById("valor")
 const display = document.getElementById("display")
 const button = document.getElementById("button")
 
-function calcularovalordosalario {
+function calcularovalordosalario() {
     const valor = Number(valorsalario.value)
-    const numero = (valor * 10) / 100
 
-    if (numero <= 2000) {
-        display.innerHTML = `<p>voce esta isento </p>`
-    } else if (numero >= 2001 && numero <= 4999) {
-
+    if (valor <= 2000) {
+        display.innerHTML = `<p> Você esta isento </p>`
+    }else if (valor > 2000 && valor <= 3500) {
+       const imposto = valor* 7.5 /100 
+       display.innerHTML = `<p> O valor do imposto é ${imposto}</p>` 
+        `<p>O salário informado foi ${valor}</p>`
+    }else if (valor > 3500 && valor <= 5000) {
+        const imposto = valor* 15 /100 
+        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>`
+        `<p> O salário informado foi ${valor}</p>`
+    }else{
+        const imposto = valor* 22.5/100
+        display.innerHTML = `<p> O valor do imposto é ${imposto}</p>`
+        `<p> O salário informado foi ${valor}</p>`
     }
+
 }
 button.addEventListener("click", calcularovalordosalario)
